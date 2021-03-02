530394561de465addd48680e2ecf4365f8eb0532
Ruan De Villiers <Ruan.DeVilliers@nintex.com>
2021-03-02 22:14:22 +0200

    Added beeceptor

--- src/components/BookingComponent.tsx
+++ src/components/BookingComponent.tsx
@@ -4,7 +4,22 @@ import './BookingComponent.css';
 function BookingComponent() {
     function onButtonClick(e) {
         e.preventDefault();
-        alert("hello");
+        // data to be sent to the POST request
+        let _data = {
+            title: "foo",
+            body: "bar",
+            userId: 1
+        }
+
+        fetch('https://ruandv.free.beeceptor.com', {
+            method: "POST",
+            body: JSON.stringify(_data),
+            headers: { "Content-type": "application/json; charset=UTF-8" }
+        })
+            .then(response => response.json())
+            .then(json => console.log(json))
+            .catch(err => console.log(err));
+
     }
     return (
         <div className="appHolder">
